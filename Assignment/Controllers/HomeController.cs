8a4387272eaef360516cbadf71c5c796da6e0c7c
Devansh Verma <devansh.verma@aepl.com>
2018-12-15 14:42:56 +0530

    initial commit

---
+++ Assignment/Controllers/HomeController.cs
@@ -0,0 +1,174 @@
+﻿//Import Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment;
+
+namespace Assignment.Controllers
+{
+
+    //class for all the products present
+    public class Product
+    {
+        static int counter = 0;
+        public int Id;
+        public string Name;
+        public string Brand;
+        public string Description;
+        public int Quantity;
+        public int Price;
+
+        //Constructor
+        public Product(string n, string b, string d, int q, int p)
+        {
+            this.Id = ++counter;
+            this.Name = n;
+            this.Brand = b;
+            this.Description = d;
+            this.Quantity = q;
+            this.Price = p;
+        }
+    };
+
+    public class HomeController : Controller
+    {
+        //static int var = 1;
+
+        //Global list of products
+        public static List<Product> products = new List<Product>(){
+            new Product("Activa", "Honda", "Scooty", 10, 50000),
+            new Product("Activa2", "Maruiti", "Scooty", 8, 50300),
+            new Product("Activa3", "BMW", "Scooty", 5, 53000)
+        };
+
+        //public void once()
+        //{
+        //    if (var == 1)
+        //    {
+        //        Product p1 = new Product("Activa", "Honda", "Scooty", 10, 50000);
+        //        Product p2 = new Product("Activa2", "Maruiti", "Scooty", 8, 50300);
+        //        Product p3 = new Product("Activa3", "BMW", "Scooty", 5, 53000);
+
+        //        products.Add(p1);
+        //        products.Add(p2);
+        //        products.Add(p3);
+
+        //        var++;
+        //    }
+
+        //    Console.Write(products.Count);
+        //}           
+
+
+        //Homepage
+        public ActionResult Index()
+        {
+            //once();
+
+            //Console.Write(products.Count);
+
+            //products.Add(p1);
+            //products.Add(p2);
+            //products.Add(p3);
+
+            return View();
+        }
+
+
+        //All Products Listing
+        public ActionResult List()
+        {
+
+            //Console.Write(products.Count);
+
+            //Product p1 = new Product("Activa", "Honda", "Scooty", 10, 50000);
+            //Product p2 = new Product("Activa2", "Maruiti", "Scooty", 8, 50300);
+            //Product p3 = new Product("Activa3", "BMW", "Scooty", 5, 53000);
+
+            //products.Add(p1);
+            //products.Add(p2);
+            //products.Add(p3);            
+
+            //@ViewBag.Items = products;
+            @ViewBag.Items = products;
+
+            return View();
+        }
+
+        //Delete a specific product
+        public ActionResult DeleteL()
+        {
+
+            return RedirectToAction("List");
+        }
+
+        //Editing a list via GET method with a specific 'pid'       
+        [HttpGet]
+        public ActionResult Edit(int pid)
+        {
+            foreach (Product p in products)
+            {
+                if (p.Id == pid)
+                {
+                    @ViewBag.prod = p;
+                }
+            }
+
+            return View();
+        }
+
+        //validation and inserting item in the list
+        [HttpPost]
+        public ActionResult Edit(int pid, string name, string description, string brand, int quantity, int price)
+        {
+            foreach (Product p in products)
+            {
+                if (p.Id == pid)
+                {
+                    p.Name = name;
+                    p.Description = description;
+                    p.Brand = brand;
+                    p.Quantity = quantity;
+                    p.Price = price;
+                }
+            }
+
+            return RedirectToAction("List");
+        }
+
+        //Add item to the list via GET method 
+        [HttpGet]
+        public ActionResult AddItems()
+        {
+
+            return View();
+        }
+
+        //Add item after validation to the list
+        [HttpPost]
+        public ActionResult AddItems(string name, string description, string brand, int quantity, int price)
+        {
+            //Validate
+            if (quantity < 0 || price < 0)
+            {
+                @ViewBag.Restore1 = name;
+                @ViewBag.Restore2 = description;
+                @ViewBag.Restore3 = brand;
+                @ViewBag.Restore4 = quantity;
+                @ViewBag.Restore5 = price;
+
+                @ViewBag.error = true;
+                return View();
+            }
+
+
+            Product p = new Product(name, brand, description, quantity, price);
+            products.Add(p);
+
+            return RedirectToAction("List");
+        }
+
+    }
+}
---
+++ Assignment/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Assignment
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //list<product> products = new list<product>();
+            //product p1 = new product("activa", "honda", "scooty", 10, 50000);
+            //product p2 = new product("activa2", "maruiti", "scooty", 8, 50300);
+            //product p3 = new product("activa3", "bmw", "scooty", 5, 53000);
+
+            //products.Add(p1);
+            //products.Add(p2);
+            //products.Add(p3);       
+
+        }
+    }  
+        
+    //public class Product{
+    //    static int counter = 0;
+    //    public int Id;
+    //    public string Name;
+    //    public string Brand;
+    //    public string Description;
+    //    public int Quantity;
+    //    public int Price;
+                
+    //    public Product(string n, string b, string d, int q, int p){
+    //        this.Id = ++counter;
+    //        this.Name = n;
+    //        this.Brand = b;
+    //        this.Description = d;
+    //        this.Quantity = q;
+    //        this.Price = p;
+    //    }
+    //}
+
+
+
+}
