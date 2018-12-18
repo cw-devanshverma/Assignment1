81fa80a9ad08ea4fbd5e473d09fee674928ad4f8
cw-devanshverma <45760644+cw-devanshverma@users.noreply.github.com>
2018-12-18 17:00:22 +0530

    Layered Architeture- (#2)
    DAL layer manages CRUD operations
    BAL layer manages validation logic
    MVC and Layered code
    
    Signed-off-by: Devansh Verma <devansh.verma@aepl.com>

--- Assignment/Controllers/HomeController.cs
+++ Assignment/Controllers/HomeController.cs
@@ -12,73 +12,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Assignment;
+using Assignment.Models;
+using Assignment.DAL.Read;
+using Assignment.BAL;
+using System.Text;
+using Assignment.DAL.Update;
+using System.Web.Routing;
+using Assignment.DAL.Create;
 
 namespace Assignment.Controllers
-{
-    //class for all the products present
-    public class Product
-    {
-        static int counter = 0;
-        public int Id;
-        public string Name;
-        public string Brand;
-        public string Description;
-        public int Quantity;
-        public int Price;
-
-        //Constructor
-        public Product(string n, string b, string d, int q, int p)
-        {
-            this.Id = ++counter;
-            this.Name = n;
-            this.Brand = b;
-            this.Description = d;
-            this.Quantity = q;
-            this.Price = p;
-        }
-    };
-
+{    
     public class HomeController : Controller
     {
         //Global list of products
-        public static List<Product> products = new List<Product>(){
+        private static List<Product> products = new List<Product>(){
             new Product("Activa", "Honda", "Scooty", 10, 50000),
             new Product("Activa2", "Maruiti", "Scooty", 8, 50300),
             new Product("Activa3", "BMW", "Scooty", 5, 53000)
         };
 
-        //Homepage
-        public ActionResult Index()
+        public List<Product> AccessProductListVariable()
         {
-            //once();
-
-            //Console.Write(products.Count);
-
-            //products.Add(p1);
-            //products.Add(p2);
-            //products.Add(p3);
-
-            return View();
+            return products;
         }
 
+        //Homepage
+        public ActionResult Index()
+        {            
+            return View();
+        }
 
         //All Products Listing
         public ActionResult List()
         {
-
-            //Console.Write(products.Count);
-
-            //Product p1 = new Product("Activa", "Honda", "Scooty", 10, 50000);
-            //Product p2 = new Product("Activa2", "Maruiti", "Scooty", 8, 50300);
-            //Product p3 = new Product("Activa3", "BMW", "Scooty", 5, 53000);
-
-            //products.Add(p1);
-            //products.Add(p2);
-            //products.Add(p3);            
-
-            //@ViewBag.Items = products;
-            @ViewBag.Items = products;
-
+            ViewBag.result = (StringBuilder)TempData["result"];
+            ViewBag.Items = (new ReadClass() ).getAllProducts();
             return View();
         }
 
@@ -91,73 +59,84 @@ namespace Assignment.Controllers
 
         //Editing a list via GET method with a specific 'pid'       
         [HttpGet]
-        public ActionResult Edit(int pid)
-        {
-            foreach (Product p in products)
-            {
-                if (p.Id == pid)
-                {
-                    @ViewBag.prod = p;
-                }
-            }
-
+        public ActionResult EditProduct(int pid){
+            ViewBag.result = (StringBuilder)TempData["result"];
+            ViewBag.prod = (new ReadClass() ).getProductById(pid);
             return View();
         }
 
         //validation and inserting item in the list
+        [ValidateInput(false)]
         [HttpPost]
-        public ActionResult Edit(int pid, string name, string description, string brand, int quantity, int price)
-        {
-            foreach (Product p in products)
-            {
-                if (p.Id == pid)
-                {
-                    p.Name = name;
-                    p.Description = description;
-                    p.Brand = brand;
-                    p.Quantity = quantity;
-                    p.Price = price;
+        public ActionResult EditProduct(int pid, string name, string description, string brand, int quantity, int price)
+        {            
+            Product prodDetails = (new ReadClass()).getProductById(pid);
+            StringBuilder status = new StringBuilder();
+
+            if (prodDetails != null){
+                // Validate
+                status = (new ValidationClass()).EditFormValidation(name, description, brand, quantity, price);
+
+                // After Successful validation                 
+                if (status.ToString() == "Record Edited Successfully!"){
+                    // Update the list
+                    (new EditClass()).editProduct(pid, name, description, brand, quantity, price);
+
+                    TempData["result"] = status;
+                    return RedirectToAction("List");
+
+                }else{
+                    // There are validation errors
+                    TempData["result"] = status;
+                    //return View("EditProduct");
+                    return RedirectToAction("EditProduct", new { pid = pid });
                 }
-            }
+            }else {
+                //ITempDataProvider has been taken care of in the views
+                //status.Append("no Product found with this id");
+                //ViewBag.result = status;
+                //@ViewBag.Items = prodDetails;
 
-            ViewBag.result = "Record Edited Successfully!";
-            @ViewBag.Items = products;
-
-            return View("List");
+                return View();
+            }            
         }
 
         //Add item to the list via GET method 
         [HttpGet]
-        public ActionResult AddItems()
+        public ActionResult AddItem()
         {
-
             return View();
         }
 
         //Add item after validation to the list
         [HttpPost]
-        public ActionResult AddItems(string name, string description, string brand, int quantity, int price)
+        public ActionResult AddItem(string name, string description, string brand, int quantity, int price)
         {
+            StringBuilder status = new StringBuilder();
+
             //Validate
-            if (quantity < 0 || price < 0)
+            status = (new ValidationClass()).EditFormValidation(name, description, brand, quantity, price);
+
+            // After Successful validation                 
+            if (status.ToString() == "Record Edited Successfully!")
             {
-                @ViewBag.name = name;
-                @ViewBag.description = description;
-                @ViewBag.brand = brand;
-                @ViewBag.quantity = quantity;
-                @ViewBag.price = price;
+                // Add the  item to list
+                (new CreateClass()).addProduct(name, description, brand, quantity, price);
 
-                @ViewBag.error = true;
-                return View();
+                ViewBag.result = "Record Inserted Successfully!";
+
+                return RedirectToAction("List");
+            }else{
+                ViewBag.name = name;
+                ViewBag.description = description;
+                ViewBag.brand = brand;
+                ViewBag.quantity = quantity;
+                ViewBag.price = price;
+
+                ViewBag.error = true;
+                return View();    
             }
-
-            ViewBag.result = "Record Inserted Successfully!";
-            @ViewBag.Items = products;
-
-            Product p = new Product(name, brand, description, quantity, price);
-            products.Add(p);
-
-            return View("List");
+            
         }
 
     }
---
+++ Assignment/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    public class Product
+    {
+        static int counter = 0;
+        public int Id;
+        public string Name;
+        public string Brand;
+        public string Description;
+        public int Quantity;
+        public int Price;
+
+        //Constructor
+        public Product(string ProdName, string ProdBrand, string ProdDescription, int ProdQuantity, int ProdPrice)
+        {
+            this.Id = ++counter;
+            this.Name = ProdName;
+            this.Brand = ProdBrand;
+            this.Description = ProdDescription;
+            this.Quantity = ProdQuantity;
+            this.Price = ProdPrice;
+        }
+
+    }
+}
